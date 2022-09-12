51412f744b18a274b8ca7c4ef2293b146cf330eb
EWA CHWEDCZUK <echwedczuk0@gmail.com>
2022-09-12 17:16:08 +0200

    add nav burger

--- src/app/components/navbar/navItems.tsx
+++ src/app/components/navbar/navItems.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import tw from 'twin.macro';
 import { slide as Menu } from 'react-burger-menu';
 import { useMediaQuery } from 'react-responsive';
 import { SCREENS } from '../responsive';
+import menuStyle from './menuStyle';
 
 const NavItems = () => {
   const isMobile = useMediaQuery({ maxWidth: SCREENS.sm });
+
+  if (isMobile)
+    return (
+      <Menu right styles={menuStyle}>
+        <ListContainer>
+          <NavItem menu>
+            <a href="#">Home</a>
+          </NavItem>
+          <NavItem menu>
+            <a href="#">Cars</a>
+          </NavItem>
+          <NavItem menu>
+            <a href="#">Services</a>
+          </NavItem>
+          <NavItem menu>
+            <a href="#">Contact Us</a>
+          </NavItem>
+        </ListContainer>
+      </Menu>
+    );
 
   return (
     <ListContainer>
@@ -35,9 +56,9 @@ const ListContainer = styled.div`
     `}
 `;
 
-const NavItem = styled.li`
+const NavItem = styled.li<{ menu?: any }>`
   ${tw`
-text-xs
+text-sm
 md:text-base
 text-black
 font-medium
@@ -49,4 +70,15 @@ duration-300
 ease-in-out
 hover:text-gray-700
 `}
+
+  ${({ menu }) =>
+    menu &&
+    css`
+      ${tw`
+    text-white
+    text-xl
+    mb-3
+    focus:text-white
+    `}
+    `}
 `;
